adcab70d54c2789c2906ce4d5492db0b3bf24e18
tianqiliang <tianqiliang>
2024-01-16 13:44:17 +0800

    first commit

--- activator/auth.py
+++ activator/auth.py
@@ -2,7 +2,7 @@ import requests
 
 
 def auth_copilot(key, machine_id, version):
-    url = "http://copilot.chatanywhere.com.cn/copilot/auth"
+    url = ""
     body = {"key": key, "id": machine_id, "version": version}
     response = requests.post(url, json=body)
     if response.status_code == 200:
